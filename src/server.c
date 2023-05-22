15de4fce640e8e3e725e8453fb16da55df14b83d
Daniel Araújo <dfdaa0@outlook.com>
2023-05-22 14:33:44 -0300

    Update server.c

--- src/server.c
+++ src/server.c
@@ -48,78 +48,78 @@ void criaServerSocket(int versaoIp, int* serverSocket) {
 }
 
 void aguardaConexao(int serverSocket, int* connectionSocket, char* nomeArquivo) {
-  if (listen(serverSocket, 5) == -1) {
-    exit(EXIT_FAILURE);
-  }
-
-  struct sockaddr_storage clientAddress;
-  socklen_t clientAddressLength = sizeof(clientAddress);
-
-  *connectionSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressLength);
-  printf("Accepted socket connection\n");
-  if (*connectionSocket == -1) {
-    exit(EXIT_FAILURE);
-  }
-
-  while (1) {
-    // Recebe o arquivo completo
-    char arquivoCompleto[BUFFER_SIZE];
-    size_t bytesRead = recv(*connectionSocket, arquivoCompleto, sizeof(arquivoCompleto) - 1, 0);
-    if (bytesRead == -1) {
-      exit(EXIT_FAILURE);
-    }
-    arquivoCompleto[bytesRead] = '\0'; // Adiciona o caractere nulo manualmente
-
-    // Remove a sequência "\end" caso esteja presente
-    size_t arquivoCompletoSize = strlen(arquivoCompleto);
-    if (arquivoCompletoSize >= 4 && strcmp(arquivoCompleto + arquivoCompletoSize - 4, "\\end") == 0) {
-      arquivoCompleto[arquivoCompletoSize - 4] = '\0';  // Remove a sequência "\end"
-    }
-
-    // Extrai o nome do arquivo
-    char* conteudoArquivo = strchr(arquivoCompleto, ':');
-    *conteudoArquivo = '\0'; // Substitui o ':' por '\0' para separar o nome do conteúdo
-    nomeArquivo = arquivoCompleto;
-
-    // Ignora o conteúdo antes do primeiro "::"
-    conteudoArquivo += 2; // Avança para o início do conteúdo
-
-    printf("Archive name received: %s\n", nomeArquivo);
-
-    if (access(nomeArquivo, F_OK) == 0) {
-      printf("%s overwritten\n", nomeArquivo);
-    } else {
-      FILE* file = fopen(nomeArquivo, "wb");
-      if (file == NULL) {
+    if (listen(serverSocket, 5) == -1) {
         exit(EXIT_FAILURE);
-      }
-      printf("Created archive in the server location\n");
+    }
 
-      size_t conteudoArquivoSize = strlen(conteudoArquivo);
+    struct sockaddr_storage clientAddress;
+    socklen_t clientAddressLength = sizeof(clientAddress);
 
-      // Escreve o conteúdo no arquivo
-      if (fwrite(conteudoArquivo, sizeof(char), conteudoArquivoSize, file) != conteudoArquivoSize) {
+    *connectionSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressLength);
+    printf("Accepted socket connection\n");
+    if (*connectionSocket == -1) {
         exit(EXIT_FAILURE);
-      }
-
-      fclose(file);
-
-      printf("%s received\n", nomeArquivo);
     }
 
-    // Adiciona o caractere nulo ao final do conteúdo do arquivo
-    size_t conteudoArquivoSize = strlen(conteudoArquivo);
-    conteudoArquivo[conteudoArquivoSize] = '\0';  // Adiciona o caractere nulo ao final do conteúdo
+    while (1) {
+        // Recebe o arquivo completo
+        char arquivoCompleto[BUFFER_SIZE];
+        size_t bytesRead = recv(*connectionSocket, arquivoCompleto, sizeof(arquivoCompleto) - 1, 0);
+        if (bytesRead == -1) {
+            exit(EXIT_FAILURE);
+        }
+        arquivoCompleto[bytesRead] = '\0'; // Adiciona o caractere nulo manualmente
 
-    // Verifica se a conexão foi fechada pelo cliente
-    if (bytesRead == 0) {
-      printf("Connection closed by client\n");
-      break; // Finaliza o programa
+        // Remove a sequência "\end" caso esteja presente
+        size_t arquivoCompletoSize = strlen(arquivoCompleto);
+        if (arquivoCompletoSize >= 4 && strcmp(arquivoCompleto + arquivoCompletoSize - 4, "\\end") == 0) {
+            arquivoCompleto[arquivoCompletoSize - 4] = '\0';  // Remove a sequência "\end"
+        }
+
+        // Extrai o nome do arquivo
+        char* conteudoArquivo = strchr(arquivoCompleto, ':');
+        *conteudoArquivo = '\0'; // Substitui o ':' por '\0' para separar o nome do conteúdo
+        nomeArquivo = arquivoCompleto;
+
+        // Ignora o conteúdo antes do primeiro "::"
+        conteudoArquivo += 2; // Avança para o início do conteúdo
+
+        printf("Archive name received: %s\n", nomeArquivo);
+
+        if (access(nomeArquivo, F_OK) == 0) {
+            printf("%s overwritten\n", nomeArquivo);
+        } else {
+            FILE* file = fopen(nomeArquivo, "wb");
+            if (file == NULL) {
+              exit(EXIT_FAILURE);
+            }
+            printf("Created archive in the server location\n");
+
+            size_t conteudoArquivoSize = strlen(conteudoArquivo);
+
+            // Escreve o conteúdo no arquivo
+            if (fwrite(conteudoArquivo, sizeof(char), conteudoArquivoSize, file) != conteudoArquivoSize) {
+                exit(EXIT_FAILURE);
+            }
+
+            fclose(file);
+
+            printf("%s received\n", nomeArquivo);
+        }
+
+        // Adiciona o caractere nulo ao final do conteúdo do arquivo
+        size_t conteudoArquivoSize = strlen(conteudoArquivo);
+        conteudoArquivo[conteudoArquivoSize] = '\0';  // Adiciona o caractere nulo ao final do conteúdo
+
+        // Verifica se a conexão foi fechada pelo cliente
+        if (bytesRead == 0) {
+            printf("Connection closed by client\n");
+            break; // Finaliza o programa
+        }
     }
-  }
 
-  // Fecha a conexão com o cliente
-  close(*connectionSocket);
+    // Fecha a conexão com o cliente
+    close(*connectionSocket);
 }
 
 int main(int argc, char* argv[]) {
